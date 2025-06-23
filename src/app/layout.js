72aebddf052c5fe838114d2a4238118bd9b291e9
Autsada Wiriya <autsadawiriya@gmail.com>
2025-06-23 19:03:59 +0700

    Update class schedule title and description in layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Class Schedule 2/67",
-  description: "Autsada Wiriya",
+  title: "Class Schedule 1/68",
+  description: "by Autsada Wiriya",
 };
 
 export default function RootLayout({ children }) {
